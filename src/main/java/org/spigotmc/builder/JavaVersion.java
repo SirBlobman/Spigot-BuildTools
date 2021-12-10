654738a65de18a644e4df6dc1497e6b6a546e930
SirBlobman <blobmansrealm@gmail.com>
2021-12-10 11:25:56 -0500

    Fix java version map.

--- src/main/java/org/spigotmc/builder/JavaVersion.java
+++ src/main/java/org/spigotmc/builder/JavaVersion.java
@@ -7,23 +7,38 @@ import lombok.Getter;
 
 @Getter
 public class JavaVersion {
+    private static final Map<Integer, JavaVersion> byVersion;
+    public static final JavaVersion JAVA_5;
+    public static final JavaVersion JAVA_6;
+    public static final JavaVersion JAVA_7;
+    public static final JavaVersion JAVA_8;
+    public static final JavaVersion JAVA_9;
+    public static final JavaVersion JAVA_10;
+    public static final JavaVersion JAVA_11;
+    public static final JavaVersion JAVA_12;
+    public static final JavaVersion JAVA_13;
+    public static final JavaVersion JAVA_14;
+    public static final JavaVersion JAVA_15;
+    public static final JavaVersion JAVA_16;
+    public static final JavaVersion JAVA_17;
     
-    //
-    public static final JavaVersion JAVA_5 = new JavaVersion("Java 5", 49);
-    public static final JavaVersion JAVA_6 = new JavaVersion("Java 6", 50);
-    public static final JavaVersion JAVA_7 = new JavaVersion("Java 7", 51);
-    public static final JavaVersion JAVA_8 = new JavaVersion("Java 8", 52);
-    public static final JavaVersion JAVA_9 = new JavaVersion("Java 9", 53);
-    public static final JavaVersion JAVA_10 = new JavaVersion("Java 10", 54);
-    public static final JavaVersion JAVA_11 = new JavaVersion("Java 11", 55);
-    public static final JavaVersion JAVA_12 = new JavaVersion("Java 12", 56);
-    public static final JavaVersion JAVA_13 = new JavaVersion("Java 13", 57);
-    public static final JavaVersion JAVA_14 = new JavaVersion("Java 14", 58);
-    public static final JavaVersion JAVA_15 = new JavaVersion("Java 15", 59);
-    public static final JavaVersion JAVA_16 = new JavaVersion("Java 16", 60);
-    public static final JavaVersion JAVA_17 = new JavaVersion("Java 17", 61);
-    private static final Map<Integer, JavaVersion> byVersion = new HashMap<Integer, JavaVersion>();
-    //
+    static {
+        byVersion = new HashMap<>();
+        JAVA_5 = new JavaVersion("Java 5", 49);
+        JAVA_6 = new JavaVersion("Java 6", 50);
+        JAVA_7 = new JavaVersion("Java 7", 51);
+        JAVA_8 = new JavaVersion("Java 8", 52);
+        JAVA_9 = new JavaVersion("Java 9", 53);
+        JAVA_10 = new JavaVersion("Java 10", 54);
+        JAVA_11 = new JavaVersion("Java 11", 55);
+        JAVA_12 = new JavaVersion("Java 12", 56);
+        JAVA_13 = new JavaVersion("Java 13", 57);
+        JAVA_14 = new JavaVersion("Java 14", 58);
+        JAVA_15 = new JavaVersion("Java 15", 59);
+        JAVA_16 = new JavaVersion("Java 16", 60);
+        JAVA_17 = new JavaVersion("Java 17", 61);
+    }
+    
     private final String name;
     private final int version;
     private final boolean unknown;
